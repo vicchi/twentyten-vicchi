4220b27ca4fc610fb330a9f493f43b3960c1aee0
vicchi <gary@vicchi.org>
2012-10-19 13:38:23 +0100

    Use the "wpb_the_biography_box" template tag in the author template in place of calling do_shortcode.

--- author.php
+++ author.php
@@ -33,25 +33,9 @@ get_header(); ?>
 				<h1 class="page-title author"><?php printf( __( 'Author Archives: %s', 'twentyten' ), "<span class='vcard'><a class='url fn n' href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a></span>" ); ?></h1>
 
 <?php
-/*
-// If a user has filled out their description, show a bio on their entries.
-
-if ( get_the_author_meta( 'description' ) ) : ?>
-					<div id="entry-author-info">
-						<div id="author-avatar">
-							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
-						</div><!-- #author-avatar -->
-						<div id="author-description">
-							<h2><?php printf( __( 'About %s', 'twentyten' ), get_the_author() ); ?></h2>
-							<?php the_author_meta( 'description' ); ?>
-						</div><!-- #author-description	-->
-					</div><!-- #entry-author-info -->
-
-<?php endif; ?>
-
-<?php
-	*/
-	echo do_shortcode ('[wp_biographia]');
+	if (function_exists ('wpb_the_biography_box')) {
+		wpb_the_biography_box ();
+	}
 	/* Since we called the_post() above, we need to
 	 * rewind the loop back to the beginning that way
 	 * we can run the loop properly, in full.
