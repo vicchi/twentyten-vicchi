b84c8d59ccd2a840e1d1bf91d180cc41aa4037e2
vicchi <gary@vicchi.org>
2012-10-22 15:39:44 +0100

    Add new custom documentation page template

--- page-documentation.php
+++ page-documentation.php
@@ -13,32 +13,44 @@
  * Template Name: Documentation
  */
 
+function vicchi_page_links () {
+	/**
+	 * Note: needs the Ambrosite Next/Previous Page Link Plus plugin to be installed
+	 * and active - http://www.ambrosite.com/plugins/next-previous-page-link-plus-for-wordpress
+	 */
+	
+	if (function_exists ('previous_page_link_plus') && function_exists ('next_page_link_plus')) {
+		$prev_format = '&larr;%link';
+		$next_format = '%link&rarr;';
+		$args = array ('format' => $prev_format, 'return' => 'output');
+		$content = array ();
+		$content[] = '<div id="page-links">';
+		$content[] = '<span class="prev-page-link">';
+		$content[] = previous_page_link_plus ($args);
+		$content[] = '</span>';
+		$content[] = '<br />';
+		$args['format'] = $next_format;
+		$content[] = '<span class="next-page-link">';
+		$content[] = next_page_link_plus ($args);
+		$content[] = '</span>';
+		$content[] = '</div>';
+		echo implode (PHP_EOL, $content);
+	}
+}
+
 get_header(); ?>
 
 		<div id="container">
 			<div id="content" role="main">
-			<?php
-			if (function_exists ('previous_page_link_plus') && function_exists ('next_page_link_plus')) {
-				$prev_format = '&larr;%link';
-				$next_format = '%link&rarr;';
-				$args = array ('format' => $prev_format, 'return' => 'output');
-				$content = array ();
-				$content[] = '<div id="page-links">';
-				$content[] = '<span class="prev-page-link">';
-				$content[] = previous_page_link_plus ($args);
-				$content[] = '</span>';
-				$content[] = '<br />';
-				$args['format'] = $next_format;
-				$content[] = '<span class="next-page-link">';
-				$content[] = next_page_link_plus ($args);
-				$content[] = '</span>';
-				$content[] = '</div>';
-				echo implode (PHP_EOL, $content);
-			}
-			get_template_part( 'loop', 'page' );
+				<?php
+				vicchi_page_links ();
+				get_template_part( 'loop', 'page' );
+				vicchi_page_links ();
 			?>
 			</div><!-- #content -->
 		</div><!-- #container -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
+?>
